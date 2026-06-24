c50b29e0f836c77ef27a0dc2cd17c96e9a738553
EzzAly74 <127905343+EzzAly74@users.noreply.github.com>
2026-06-24 13:12:20 +0300

    update analyser swagger l5

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -129,10 +129,7 @@ return [
              *
              * @see \OpenApi\scan
              */
-            'analyser' => new \OpenApi\Analysers\ReflectionAnalyser([
-                new \OpenApi\Analysers\AttributeAnnotationFactory(),
-                new \OpenApi\Analysers\DocBlockAnnotationFactory(),
-            ]),
+            'analyser' => null,
 
             /**
              * analysis: defaults to a new \OpenApi\Analysis .
